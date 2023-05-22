22533cccc029cf514bbc7d0fb950d08efe97b948
estela-polverini <estela.polverini@sptech.school>
2023-05-21 21:14:48 -0300

    aaaaa

--- projeto-individual/src/main/java/yaaaay/projeto/individual/s3/App.java
+++ projeto-individual/src/main/java/yaaaay/projeto/individual/s3/App.java
@@ -8,6 +8,7 @@ package yaaaay.projeto.individual.s3;
 import com.github.britooo.looca.api.core.Looca;
 
 import java.util.*;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 /**
@@ -17,20 +18,31 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class App {
     public static void main(String[] args) {
         ConexaoBancoLocal conexaoBanco = new ConexaoBancoLocal();
-        JdbcTemplate jdbc =  conexaoBanco.getConnection();
+        JdbcTemplate conexao =  conexaoBanco.getConnection();
+        ConexaoBancoAzure conexaoAzure = new ConexaoBancoAzure();
+        JdbcTemplate conexaosql =  conexaoAzure.getConnection();
         Scanner leitor01 = new Scanner(System.in);
 
         Looca looca = new Looca();
         
         Runnable deletarTabela = () -> {
-            jdbc.execute("DROP TABLE IF EXISTS Computador_EGP;");
+            conexao.execute("DROP TABLE IF EXISTS Computador_EGP;");
+            conexaosql.execute("DROP TABLE IF EXISTS Computador_EGP;");
         };
         deletarTabela.run();
         
         Runnable criarTabela = () -> {
-            jdbc.execute("""
+            conexao.execute("""
                          CREATE TABLE Computador_EGP (
                              idComputador INT AUTO_INCREMENT,
+                             sistema_operacional VARCHAR(45),
+                             modelo VARCHAR(45),
+                             total_armazenamento VARCHAR(45),
+                             PRIMARY KEY (idComputador)
+                         );""");
+            conexaosql.execute("""
+                         CREATE TABLE Computador_EGP (
+                             idComputador INT IDENTITY(1,1),
                              sistema_operacional VARCHAR(45),
                              modelo VARCHAR(45),
                              total_armazenamento VARCHAR(45),
@@ -41,7 +53,12 @@ public class App {
         
         System.out.println("Bem vindo ao Sistema Hemera Tech!");
         
-        jdbc.update("INSERT INTO Computador_EGP(sistema_operacional, modelo, total_armazenamento) VALUES (?, ?, ?)",
+        conexao.update("INSERT INTO Computador_EGP(sistema_operacional, modelo, total_armazenamento) VALUES (?, ?, ?)",
+                            looca.getSistema().getSistemaOperacional(), 
+                            looca.getSistema().getFabricante(),
+                            looca.getGrupoDeDiscos().getTamanhoTotal());
+        
+        conexaosql.update("INSERT INTO Computador_EGP(sistema_operacional, modelo, total_armazenamento) VALUES (?, ?, ?)",
                             looca.getSistema().getSistemaOperacional(), 
                             looca.getSistema().getFabricante(),
                             looca.getGrupoDeDiscos().getTamanhoTotal());
@@ -55,8 +72,15 @@ public class App {
         System.out.println("Agora insira sua senha:");
         String senha = leitor01.nextLine();
         
+        String selectUsuario = "select Empresa.nome as nomeEmpresa,Empresa.idEmpresa,Funcionario.nome,Funcionario.sobrenome,Funcionario.email,Funcionario.senha, Funcao.funcao from Funcionario join Funcao on Funcionario.idFuncionario = Funcao.idFuncionario join Empresa on Empresa.idEmpresa = Funcao.idEmpresa where Funcionario.email = ? and Funcionario.senha= ?";
+        ObjUsuario usuarioLogadoAzure = conexaosql.queryForObject(selectUsuario, new BeanPropertyRowMapper<>(ObjUsuario.class), email, senha);
+        ObjUsuario usuarioLogadoMySql = conexao.queryForObject(selectUsuario, new BeanPropertyRowMapper<>(ObjUsuario.class), email, senha);
         
-        
-        
+        if (usuarioLogadoAzure == null || usuarioLogadoMySql == null) {
+            System.out.println("Usuário ou senha incorretos");
+        } else {
+            System.out.println("Login realizado com sucesso");
+            
+        }
     }
 }
